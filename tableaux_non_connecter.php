16fcff5a2113db2259015e9522c4c60a1bff68d5
rorog2 <rorog2@live.fr>
2016-12-24 19:51:45 +0100

    Mise en place du design du site et de la page d'accueil et création d'un fichier avec une structure d'exemple

---
+++ tableaux_non_connecter.php
@@ -0,0 +1,6 @@
+<ul class="navigation">
+    <h2>Menu</h2>
+    <li class="navigation"><a href="index.php" class="navigation">Acceuil</a></li>
+    <li class="navigation"><a href="index.php" class="navigation">Calcul</a></li>
+    <li class="navigation"><a href="index.php" class="navigation">Se connecter</a></li>
+</ul>
