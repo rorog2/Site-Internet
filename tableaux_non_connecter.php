173737e7c2c32c81a46213c9f4ebd371341be866
rorog2 <rorog2@live.fr>
2016-12-26 18:19:08 +0100

    Ajout de quelque ligne dans le tableau

--- tableaux_non_connecter.php
+++ tableaux_non_connecter.php
@@ -4,6 +4,8 @@
     <li class="navigation"><a href="index.php" class="navigation">Calcul</a></li>
     <li class="navigation"><a href="index.php" class="navigation">Composants</a></li>
     <li class="navigation"><a href="index.php" class="navigation">Chats</a></li>
+    <li class="navigation"><a href="index.php" class="navigation">Nos logiciels</a></li>
+    <li class="navigation"><a href="index.php" class="navigation">A propos</a></li>
     <li class="navigation"><a href="administration.php" class="navigation">Se connecter</a></li>
     <li class="navigation"><a href="administration.php?inscription" class="navigation">Inscription</a></li>
 </ul>
