53021541c9b1cd22f3c60adabd778394e20f5b39
Romain Guitteau--Giroire <rorog2@live.fr>
2017-05-17 15:11:50 +0200

    Commencement creation page automatique BDD

---
+++ creationpage.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require('affichage_nom.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<!-- Onglet -->
+    <head>
+        <meta charset="utf-8" />
+        <title>Electron's ique</title>
+        <link rel="icon" type="image/png" href="images/logo.png" />
+        <link rel="stylesheet" type="text/css" href="css/style_pc.css" />
+    </head>
+
+<!-- Corps de la page -->
+    <body>
+        <!-- Tete de page -->
+        <header>
+            <img src="images/logo.png" alt="Logo du site" id="logo_baniere" />
+            <h1 style="display: inline-block;">Electron's ique</h1>
+        </header>
+
+        <!-- Navigation -->
+        <nav>
+            <?php include('affichage_tableaux.php'); ?>
+        </nav>
+        
+        <!-- Page -->
+        <div class="general">
+        <?php
+            include('creationpage/creation_page.php');
+        ?>
+        </div>
+
+        <!-- Pied de page -->
+        <footer>
+            <p id="centrer_texte" class="footer">Vous pouvez nous retrouver sur les réseaux sociaux !</p>
+        </footer>
+<!-- Fin de la page -->  
+    </body>
+    <script src="creationpage/script.js"></script>
+</html>
