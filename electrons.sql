39fadd54a42179c068c46f1e73e5aa59c5a13d2b
rorog2 <rorog2@live.fr>
2017-07-29 20:23:57 +0200

    Changement de la structure de base des pages stockées dans la base de données

--- electrons.sql
+++ electrons.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.2.12deb2+deb8u2
+-- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Mer 22 Mars 2017 à 15:17
--- Version du serveur :  5.7.14
--- Version de PHP :  5.6.25
+-- Client :  localhost
+-- Généré le :  Sam 29 Juillet 2017 à 16:27
+-- Version du serveur :  5.5.55-0+deb8u1
+-- Version de PHP :  5.6.30-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `electrons`
@@ -26,8 +26,8 @@ SET time_zone = "+00:00";
 -- Structure de la table `administration`
 --
 
-CREATE TABLE `administration` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `administration` (
+`id` int(11) NOT NULL,
   `pseudo` varchar(255) NOT NULL,
   `mdp` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE `administration` (
   `prenom` varchar(255) NOT NULL,
   `changer_mdp` int(11) NOT NULL,
   `desactiver` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `administration`
@@ -51,12 +51,19 @@ INSERT INTO `administration` (`id`, `pseudo`, `mdp`, `email`, `nom`, `prenom`, `
 -- Structure de la table `arduino`
 --
 
-CREATE TABLE `arduino` (
-  `id` int(11) NOT NULL,
-  `contenu` text NOT NULL,
-  `paragraphe` int(11) NOT NULL,
-  `important` tinyint(1) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `arduino` (
+`id` int(11) NOT NULL,
+  `balise` varchar(15) NOT NULL,
+  `attribut` varchar(255) NOT NULL,
+  `contenu` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `arduino`
+--
+
+INSERT INTO `arduino` (`id`, `balise`, `attribut`, `contenu`) VALUES
+(1, 'h1', '', 'Les Arduino');
 
 -- --------------------------------------------------------
 
@@ -64,8 +71,8 @@ CREATE TABLE `arduino` (
 -- Structure de la table `chat`
 --
 
-CREATE TABLE `chat` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `chat` (
+`id` int(11) NOT NULL,
   `pseudo` varchar(50) NOT NULL,
   `message` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table contenant les messages du chat';
@@ -76,7 +83,7 @@ CREATE TABLE `chat` (
 -- Structure de la table `connectes`
 --
 
-CREATE TABLE `connectes` (
+CREATE TABLE IF NOT EXISTS `connectes` (
   `ip` varchar(15) NOT NULL,
   `timestamp` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -89,19 +96,19 @@ CREATE TABLE `connectes` (
 -- Index pour la table `administration`
 --
 ALTER TABLE `administration`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `arduino`
 --
 ALTER TABLE `arduino`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `chat`
 --
 ALTER TABLE `chat`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -111,17 +118,17 @@ ALTER TABLE `chat`
 -- AUTO_INCREMENT pour la table `administration`
 --
 ALTER TABLE `administration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `arduino`
 --
 ALTER TABLE `arduino`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
