6796df746756fd7a364e4e23499daeb51fb5ca44
rorog2 <rorog2@live.fr>
2017-12-29 23:35:59 +0100

    Nouvelle version du style de l'aide

--- aide/_translations.js
+++ aide/_translations.js
@@ -1,11 +1,11 @@
 
 function hnd_ut(a){
-a.TRANSLATIONS['Search term too short'] = "Search term too short";
-a.TRANSLATIONS['No results'] = "No results";
-a.TRANSLATIONS['Please enter 3 or more characters'] = "Please enter 3 or more characters";
-a.TRANSLATIONS['Word list not ready yet. Please wait until the word list is fully downloaded'] = "Word list not ready yet. Please wait until the word list is fully downloaded";
-a.TRANSLATIONS['Incorrect or corrupt search data. Please check your HelpNDoc template'] = "Incorrect or corrupt search data. Please check your HelpNDoc template";
+a.TRANSLATIONS['Search term too short'] = "Terme de recherche trop court";
+a.TRANSLATIONS['No results'] = "Pas de résultats";
+a.TRANSLATIONS['Please enter 3 or more characters'] = "S'il vous plais entrez 3 ou plus de caractères";
+a.TRANSLATIONS['Word list not ready yet. Please wait until the word list is fully downloaded'] = "La liste des mots n'est pas encore prête. Veuillez patienter jusqu'à ce que la liste de mots soit entièrement téléchargée";
+a.TRANSLATIONS['Incorrect or corrupt search data. Please check your HelpNDoc template'] = "Données de recherche incorrectes ou corrompues. Veuillez vérifier votre modèle HelpNDoc";
 a.TRANSLATIONS['Related topics...'] = "Related topics...";
-a.TRANSLATIONS['Loading...'] = "Loading...";
-a.TRANSLATIONS['Close'] = "Close";
+a.TRANSLATIONS['Loading...'] = "Chargement...";
+a.TRANSLATIONS['Close'] = "Fermer";
 }
