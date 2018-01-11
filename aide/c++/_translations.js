44606067bac5740fcfde857f113f06ca551afb5a
rorog2 <rorog2@live.fr>
2018-01-11 16:50:21 +0100

    Nouvelle version de l'aide et fihcier d'aide compiler en chm

--- aide/c++/_translations.js
+++ aide/c++/_translations.js
@@ -5,7 +5,7 @@ a.TRANSLATIONS['No results'] = "Pas de résultats";
 a.TRANSLATIONS['Please enter 3 or more characters'] = "S'il vous plais entrez 3 ou plus de caractères";
 a.TRANSLATIONS['Word list not ready yet. Please wait until the word list is fully downloaded'] = "La liste des mots n'est pas encore prête. Veuillez patienter jusqu'à ce que la liste de mots soit entièrement téléchargée";
 a.TRANSLATIONS['Incorrect or corrupt search data. Please check your HelpNDoc template'] = "Données de recherche incorrectes ou corrompues. Veuillez vérifier votre modèle HelpNDoc";
-a.TRANSLATIONS['Related topics...'] = "Related topics...";
+a.TRANSLATIONS['Related topics...'] = "Rubriques connexes...";
 a.TRANSLATIONS['Loading...'] = "Chargement...";
 a.TRANSLATIONS['Close'] = "Fermer";
 }
